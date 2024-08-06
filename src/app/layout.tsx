84d679564c5716a5e98e777593a0d28821ae9650
shubhamxshah <ritikgujarat@gmail.com>
2024-08-06 14:57:36 +0530

    pushed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,8 +10,8 @@ import { BillingProvider } from '@/providers/billing-provider'
 const font = DM_Sans({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Fuzzie.',
-  description: 'Automate Your Work With Fuzzie.',
+  title: 'Buzzie.',
+  description: 'Automate Your Work With Buzzie.',
 }
 
 export default function RootLayout({
